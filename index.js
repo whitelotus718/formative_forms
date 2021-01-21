5b1703b02b512c9c9253856154a734f76e6c8e95
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-01-21 18:07:47 -0500

    phase 1 complete

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index", users );
+  res.render("index", {users} );
 });
 
 const users = [
